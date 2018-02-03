59942a34cc2d6e349ff4ea649803a3abd99c40c6
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-02-03 14:20:55 -0500

    fix comment

--- src/cis/Resources.java
+++ src/cis/Resources.java
@@ -16,7 +16,7 @@ public class Resources {
 	//Shared port between client and intermediate host
 	public static final int clientPort = 23;
 	
-	//Shared port between client and intermediate host
+	//Shared port between intermediate host and server
 	public static final int serverPort = 69;
 
 	public static final int timeout = 100000;
